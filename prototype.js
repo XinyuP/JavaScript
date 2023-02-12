404cf13ae2631615075b771164aadac14c955abf
XinyuP <xinyup@umich.edu>
2023-02-11 19:55:40 -0500

    arrow func + prototype

---
+++ prototype.js
@@ -0,0 +1,21 @@
+let log = console.log;
+
+let myObj = {}; // new Object()
+log(myObj.constructor); // function Object(){}
+log(myObj.__proto__ === myObj.constructor.prototype);
+
+function Cat() {
+    //constructor for kitty
+}
+let kitty = new Cat();
+log(kitty.__proto__ === Cat.prototype);
+log(kitty.__proto__.__proto__ === Object.prototype);
+log(Object.prototype.__proto__);
+
+function Animal() {}
+
+Object.setPrototypeOf(Cat.prototype, Animal.prototype);
+log(kitty.__proto__); // Cat{}
+log(kitty.__proto__.__proto__); // Animal {}
+log(kitty.__proto__.__proto__.__proto__); // {}
+log(kitty.__proto__.__proto__.__proto__.__proto__); // null
